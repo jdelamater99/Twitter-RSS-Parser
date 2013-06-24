84051692d25a894f374abfe38edbc623078e3ac0
jdelamater99 <jeff.delamater@yahoo.com>
2013-06-24 12:07:16 -0300

    Create search.php

---
+++ search.php
@@ -0,0 +1,49 @@
+<?php
+//v1.0
+
+$host = 'api.twitter.com';
+$method = 'GET';
+$path = '/1.1/search/tweets.json'; // api call path
+
+$query = array( // query parameters
+  'q' => $q,
+	'include_entities' => $search_include_entities,
+	'result_type' => $search_result_type
+);
+
+include "functions.php";
+
+print('<?xml version="1.0" encoding="utf-8"?>'. PHP_EOL);
+print('<?xml-stylesheet type="text/xsl" href="atom-to-html.xsl"?>'. PHP_EOL);
+
+print('<feed xmlns="http://www.w3.org/2005/Atom">'. PHP_EOL);
+print('<title>Search: '. $q . '</title>'. PHP_EOL);
+
+$arrLen = count($twitter_data['statuses']);
+for ($i=0; $i<$arrLen; $i++) {
+	print(PHP_EOL. '	<entry>'. PHP_EOL);
+		print('		<id>https://twitter.com/'.$twitter_data['statuses'][$i]['user']['screen_name'].'/statuses/'. $twitter_data['statuses'][$i]['id_str'] .'</id>'. PHP_EOL);
+		print('		<link href="https://twitter.com/'.$twitter_data['statuses'][$i]['user']['screen_name'].'/statuses/'. $twitter_data['statuses'][$i]['id_str'] .'" rel="alternate" type="text/html"/>'. PHP_EOL);
+		print('		<title>'.$twitter_data['statuses'][$i]['user']['screen_name'].': '.htmlspecialchars($twitter_data['statuses'][$i]['text']).'</title>'. PHP_EOL);
+		print('		<summary type="html"><![CDATA['.$twitter_data['statuses'][$i]['user']['screen_name'].': '.$twitter_data['statuses'][$i]['text'].']]></summary>'. PHP_EOL);
+		
+		$feedContent = '		<content type="html"><![CDATA[<html><body><p></p><p>'.$twitter_data['statuses'][$i]['text'].'</p></body></html>]]></content>';
+		$text = processString($feedContent);
+		
+		print($text . PHP_EOL);
+		print('		<updated>'.$twitter_data['statuses'][$i]['created_at'].'</updated>'. PHP_EOL);
+		print('		<author><name></name></author>'. PHP_EOL);
+		
+		$hashLen = count($twitter_data['statuses'][$i]['entities']['hashtags']);
+		if ($hashLen > 0){
+			for ($j=0; $j<$hashLen; $j++){
+				print('		<category term="'.$twitter_data['statuses'][$i]['entities']['hashtags'][$j]['text'].'"/>'. PHP_EOL);
+			}
+		}
+		
+	print('	</entry>'. PHP_EOL);
+}
+
+print('</feed>'. PHP_EOL);
+print('<!-- vim:ft=xml -->');
+?>
