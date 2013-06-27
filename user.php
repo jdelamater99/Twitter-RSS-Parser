73d879a5b49fc5343a510de7840f15d0f4041cc8
craywolf <craywolf@example.com>
2013-06-27 10:49:00 -0400

    Several id and link tag fixes

--- user.php
+++ user.php
@@ -24,14 +24,16 @@ if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off'
     $protocol = 'http://';
 }
 
+header('Content-type: application/atom+xml; charset=utf-8');
+
 print('<?xml version="1.0" encoding="utf-8"?>'. PHP_EOL);
 print('<feed xmlns="http://www.w3.org/2005/Atom" xml:lang="en" xml:base="'.$_SERVER['SERVER_NAME'].'">'. PHP_EOL);
 
-print('<id>https://twitter.com/'.$twitter_data[0]['user']['screen_name'].'/statuses/'. $twitter_data[0]['id_str'] .'</id>'. PHP_EOL);
+print('<id>tag:twitter.com,2006:/'.$twitter_data[0]['user']['screen_name'].'</id>'. PHP_EOL);
 print('<title>@'.$twitter_data[0]['user']['screen_name'].'</title>'. PHP_EOL);
 print('<updated>'.date('c', strtotime($twitter_data[0]['created_at'])).'</updated>'. PHP_EOL);
 
-print('<link href="https://twitter.com/'.$twitter_data[0]['user']['screen_name'].'" rel="alternate" type="application/atom+xml"/>'. PHP_EOL);
+print('<link href="https://twitter.com/'.$twitter_data[0]['user']['screen_name'].'"/>'. PHP_EOL);
 print('<link href="'.$protocol.$_SERVER['SERVER_NAME'].str_replace("&", "&amp;", $_SERVER['REQUEST_URI']).'" rel="self" type="application/atom+xml" />'. PHP_EOL);
 
 include "feed.php";
