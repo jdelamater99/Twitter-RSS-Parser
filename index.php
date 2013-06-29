2d38e1014d4dff6cb1f8c3ec84270bdc00e44300
craywolf <craywolf@example.com>
2013-06-28 23:58:52 -0400

    Fix/add comments in index.php

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-/**
- * Make sure config.php exists before loading it
- */
+// * Make sure config.php exists before loading it
 if(file_exists("config.php")) {
 	include "config.php";
 } else {
@@ -9,16 +7,23 @@ if(file_exists("config.php")) {
 	exit();
 }
 
+// * Explicitly advertise that this is XML so browsers don't style it
 if (!isset($_GET['test'])){
 	header('Content-type: application/atom+xml; charset=utf-8');
 }
 
+// * Fetch $count from the URL if the user supplied it
 if (isset($_GET["count"])) {
 	if (is_int(intval($_GET["count"]))) {
 		$session_count = intval( $_GET["count"] );
 	}
 }
 
+// * Now, based on what variables are in the URL, pick the type
+// * Of query we're going to do.
+// *
+// * Note these use the ternary operator (?:) to keep things short.
+// * It works like: (if this is true) ? (then return this) : (else return this)
 if (isset( $_GET["screen_name"])) {
 	$screen_name = htmlspecialchars($_GET["screen_name"]);
 	$count = isset($session_count) ? $session_count : $user_count;
