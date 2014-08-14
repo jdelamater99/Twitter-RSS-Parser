af5556997f16f1950770ed7c56ab0cf0f5d352e6
Miłosz Sieradzki <milosz+github@sieradzki.it>
2014-08-14 20:33:47 +0200

    Fix newlines in tweets content

--- feed.php
+++ feed.php
@@ -11,21 +11,21 @@ for ($i=0; $i<$arrLen; $i++) {
 		print('		<link href="https://twitter.com/'.$td[$i]['user']['screen_name'].'/statuses/'. $td[$i]['id_str'] .'" rel="alternate" type="text/html"/>'. PHP_EOL);
 		print('		<title>'.$td[$i]['user']['screen_name'].': '.htmlspecialchars($td[$i]['text']).'</title>'. PHP_EOL);
 		print('		<summary type="html"><![CDATA['.$td[$i]['user']['screen_name'].': '.$td[$i]['text'].']]></summary>'. PHP_EOL);
-		
-		$feedContent = '		<content type="html"><![CDATA[<p>'.$td[$i]['text'].'</p>]]></content>';
+
+		$feedContent = '		<content type="html"><![CDATA[<p>'.nl2br($td[$i]['text']).'</p>]]></content>';
 		$text = processString($feedContent);
-		
+
 		print($text . PHP_EOL);
 		print('		<updated>'.date('c', strtotime($td[$i]['created_at'])).'</updated>'. PHP_EOL);
 		print('		<author><name>'.$td[$i]['user']['screen_name'].'</name></author>'. PHP_EOL);
-		
+
 		$hashLen = count($td[$i]['entities']['hashtags']);
 		if ($hashLen > 0){
 			for ($j=0; $j<$hashLen; $j++){
 				print('		<category term="'.$td[$i]['entities']['hashtags'][$j]['text'].'"/>'. PHP_EOL);
 			}
 		}
-		
+
 	print('	</entry>'. PHP_EOL);
 }
 
